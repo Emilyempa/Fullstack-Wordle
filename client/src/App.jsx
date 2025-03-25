bf9220eaf33146d2c350795d4f74abcbe83b3c9d
Emilyempa <emilypettersson@hotmail.com>
2025-03-25 15:18:40 +0100

    Add theme and header

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-
+import Header from '/src/components/Header.jsx';
+import theme from './theme.js';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
 function App() {
   
 
   return (
-    <>
-      <h1>Wordle</h1>
-    </>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Header/>
+    </ThemeProvider>
   )
 }
 
---
+++ client/src/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#cdb4db', 
+    },
+    secondary: {
+      main: '#003049', 
+    },
+    background: {
+      default: '#edede9',
+    },
+  },
+});
+
+export default theme;
