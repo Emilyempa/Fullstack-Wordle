35a6ea78d07657a306406291f085df4b2c4649d6
Emilyempa <emilypettersson@hotmail.com>
2025-03-27 10:43:00 +0100

    Fix communication between client and server

--- client/src/pages/Play.jsx
+++ client/src/pages/Play.jsx
@@ -1,7 +1,29 @@
+import { useEffect, useState } from "react";
+
 function Play() {
+  const [message, setMessage] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await fetch('/api/test-endpoint');
+        if (!res.ok) {
+          throw new Error(`HTTP error! Status: ${res.status}`);
+        }
+        const data = await res.json();
+        setMessage(data.message);
+      } catch (error) {
+        console.error('Error fetching message:', error);
+      }
+    };
+    fetchData(); 
+  }, []); 
+
+
   return (
     <div>
       <h1>Play</h1>
+      <p>{message}</p>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
 const port = 5080;
-
+// const cors = require('cors');
 // app.set('view engine', 'ejs');
-
+// app.use(cors());
 
 // app.use(express.static(path.join(__dirname, '../client/dist')));
 // app.get('/', (req, res) => {
@@ -13,6 +13,11 @@ const port = 5080;
 app.get('/', (req, res) => {
     res.send('<h1>Hello, server!</h1>');
   });
+
+app.get('/api/test-endpoint', (req, res) => {
+  const resMessage = { message: 'Hello from the server!' };
+  res.json(resMessage);
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
