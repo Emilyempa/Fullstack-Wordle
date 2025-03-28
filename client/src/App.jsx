3c06c92a2b4f176f79dd70f3368a977ef92f13a0
Emilyempa <emilypettersson@hotmail.com>
2025-03-28 13:17:00 +0100

    Add box to center content

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import Footer from '/src/components/Footer.jsx';
 import theme from './theme.js';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Play from '/src/pages/Play.jsx';
 import About from '/src/pages/About.jsx';
@@ -14,11 +15,21 @@ function App() {
       <CssBaseline />
       <Router>
         <Header />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            minHeight: "calc(100vh - 120px)", 
+            padding: 2,
+          }}
+        >
         <Routes>
           <Route path="/" element={<Play />} /> 
           <Route path="/about" element={<About />} />
           <Route path="*" element={<NotFound />} /> 
         </Routes>
+        </Box>
         <Footer />
       </Router>
     </ThemeProvider>
