d68558c963f5ba1d298d6e76e731d695ac0d692c
Emilyempa <emilypettersson@hotmail.com>
2025-04-08 14:56:08 +0200

    Remove unused state and API call from Play component

--- client/src/pages/Play.jsx
+++ client/src/pages/Play.jsx
@@ -1,13 +1,11 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import WordList from "../components/WordList.jsx";
 import DropDown from "../components/Dropdown.jsx";
 import RepeatControl from "../components/RepeatControl.jsx";
-import { fetchApiData } from "../api/apiFetch.js";
 import TextBox from "../components/TextBox.jsx";
 import StartButton from "../components/StartButton.jsx";
 
 function Play() {
-  const [message, setMessage] = useState("");
   const [selectedNumber, setSelectedNumber] = useState(2);
   const [allowRepeats, setAllowRepeats] = useState(true);
   const [correctWord, setCorrectWord] = useState("");
@@ -16,18 +14,6 @@ function Play() {
   const handleRepeatChange = (value) => {
     setAllowRepeats(value);
   };
-
-  useEffect(() => {
-    const getMessage = async () => {
-      try {
-        const data = await fetchApiData("/api/test-endpoint");
-        setMessage(data.message);
-      } catch (error) {
-        console.error("Error:", error);
-      }
-    };
-    getMessage();
-  }, []);
 
   return (
     <div>
@@ -48,7 +34,6 @@ function Play() {
             allowRepeats={allowRepeats}
             onWordFetched={setCorrectWord}
           />
-          <p>{message}</p>
         </>
       )}
     </div>
