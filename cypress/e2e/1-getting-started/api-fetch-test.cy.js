d2d6dbb337b5eb9f9eb180e10df4ee93390f479e
Emilyempa <emilypettersson@hotmail.com>
2025-04-15 16:31:18 +0200

    Add test for fetching word with length 8 and allow repeats in API tests

--- cypress/e2e/1-getting-started/api-fetch-test.cy.js
+++ cypress/e2e/1-getting-started/api-fetch-test.cy.js
@@ -24,4 +24,23 @@ describe("wordle API test", () => {
       expect(interception.response.statusCode).to.equal(200);
     });
   });
+
+  it("tests if word fetch works and gets word length 8 and repeat true", () => {
+    cy.reload();
+    cy.intercept("GET", "/api/random-word?length=8&allowRepeats=true").as(
+      "fetchWordTrue"
+    );
+       
+    cy.get("#number-select").should("contain.text", "2").click();
+    cy.get('[data-value="8"]').click();    
+    cy.get(":nth-child(1) > .MuiButton-root").click();
+    cy.wait(1000);
+    cy.wait("@fetchWordTrue").then((interception) => {
+      expect(interception.request.url).to.contain("length=8");
+      expect(interception.request.url).to.contain("allowRepeats=true");
+      expect(interception.response.statusCode).to.equal(200);
+    });
+  });
 });
+
+
